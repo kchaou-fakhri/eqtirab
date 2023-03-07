5133652b8f1a33fb7e6c5f4a7390415bb77634a4
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-03-07 18:41:33 +0100

    feat() : Hez screen - implements mp3 player feat #feature/takrir_ayate

--- app/src/main/java/com/megahed/eqtarebmenalla/feature_data/presentation/ui/quranListenerReader/ListenerHelperFragment.kt
+++ app/src/main/java/com/megahed/eqtarebmenalla/feature_data/presentation/ui/quranListenerReader/ListenerHelperFragment.kt
@@ -1,25 +1,31 @@
 package com.megahed.eqtarebmenalla.feature_data.presentation.ui.quranListenerReader
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.arges.sepan.argmusicplayer.Models.ArgAudio
+import com.arges.sepan.argmusicplayer.Models.ArgAudioList
 import com.megahed.eqtarebmenalla.common.Constants
 import com.megahed.eqtarebmenalla.databinding.FragmentListenerHelperBinding
+import com.megahed.eqtarebmenalla.feature_data.data.remote.hez.entity.Reway
 import com.megahed.eqtarebmenalla.feature_data.presentation.viewoModels.HefzVM
 import dagger.hilt.android.AndroidEntryPoint
 
 
-@AndroidEntryPoint
+
 class ListenerHelperFragment : Fragment() {
 
     private lateinit var binding: FragmentListenerHelperBinding
     private lateinit var hefzVM: HefzVM
+    lateinit var mediaPlayer: MediaPlayer
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,11 +34,15 @@ class ListenerHelperFragment : Fragment() {
     ): View {
         binding = FragmentListenerHelperBinding.inflate(inflater, container, false)
         val root: View = binding.root
+            mediaPlayer = MediaPlayer()
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
 
           hefzVM  = HefzVM()
         var arrSura = arrayListOf<String>()
         var arrEya = arrayListOf<Int>()
         var arrEyaEnd = arrayListOf<Int>()
+        var tempRewat = arrayListOf<Reway>()
+        var tempSuraId = 1
 
         val adapterSura = ArrayAdapter(requireContext(),
             android.R.layout.simple_list_item_1, arrSura)
@@ -50,6 +60,7 @@ class ListenerHelperFragment : Fragment() {
         // Display list of rewat
         hefzVM.getAllRewat().observe(viewLifecycleOwner, Observer {
 
+            tempRewat.addAll(it.data)
                 var arrRewat = arrayListOf<String>()
                  it.data.forEach{
                      if (it.language.equals("ar")){
@@ -92,6 +103,7 @@ class ListenerHelperFragment : Fragment() {
                 for (list in arrSura) {
                     if (list.toString().equals(adapterView.getItemAtPosition(position))) {
                         arrEya.clear()
+                        tempSuraId = position+1
                         for (i in 1..Constants.SORA_OF_QURAN_WITH_NB_EYA.get(list.toString())!!){
                             arrEya.add(i)
 
@@ -110,9 +122,8 @@ class ListenerHelperFragment : Fragment() {
                 for (list in arrEya) {
                     if (list == adapterView.getItemAtPosition(position).toString().toInt()) {
                         arrEyaEnd.clear()
-                        Toast.makeText(requireContext(), list.toString(), Toast.LENGTH_LONG).show()
-            var max = Constants.SORA_OF_QURAN_WITH_NB_EYA.get(binding.listSouraName.text.toString())
-                        for (i in list+1 ..max!!){
+                        var max = Constants.SORA_OF_QURAN_WITH_NB_EYA.get(binding.listSouraName.text.toString())
+                        for (i in list ..max!!){
                             arrEyaEnd.add(i)
 
 
@@ -125,9 +136,86 @@ class ListenerHelperFragment : Fragment() {
                 }
             })
 
+        binding.nbEyaEnd.setOnItemClickListener(
+            OnItemClickListener { adapterView, view, position, id ->
+                for (list in arrEyaEnd) {
+                    if (list == adapterView.getItemAtPosition(position).toString().toInt()) {
+
+                        binding.start.isEnabled = true
+
+
+                    }
+                }
+            })
 
 
 
+
+        binding.start.setOnClickListener {
+            var rewayIdSelected = ""
+           tempRewat.forEach {
+               if (it.name.equals(binding.listOfRewat.text.toString())){
+                   rewayIdSelected = it.identifier
+
+               }
+           }
+
+
+            hefzVM.getSuraMp3(tempSuraId ,rewayIdSelected).observe(viewLifecycleOwner, Observer {
+                val playlist = ArgAudioList(true)
+                var audio : ArgAudio
+
+                    for(i in binding.nbAya.text.toString().toInt() ..binding.nbEyaEnd.text.toString().toInt()){
+                        for (j in 0..binding.nbAyaRepeat.text.toString().toInt()){
+                            audio = ArgAudio.createFromURL(it.data.ayahs.get(i).text,
+                                it.data.ayahs.get(i).number.toString(),  it.data.ayahs.get(i).audio)
+                            playlist.add(audio)
+                        }
+
+
+
+
+
+
+
+
+
+                }
+
+                binding.argmusicplayer.playPlaylist(playlist );
+
+            //                for (i in 0..binding.suraRepeat.text.toString().toInt()){
+//
+//                    it.data.ayahs.forEach {
+//                       for (j in binding.nbAya.text.toString().toInt()
+////                           .. binding.nbEyaEnd.text.toString().toInt()){
+//                           try {
+//                               // on below line we are setting audio
+//                               // source as audio url on below line.
+//                               mediaPlayer.setDataSource(it.audio)
+//
+//                               // on below line we are
+//                               // preparing our media player.
+//                               mediaPlayer.prepare()
+//
+//                               // on below line we are
+//                               // starting our media player.
+//                               mediaPlayer.start()
+//
+//                           } catch (e: Exception) {
+//
+//                               // on below line we are handling our exception.
+//                               e.printStackTrace()
+//                           }
+//                           // on below line we are displaying a toast message as audio player.
+//                           Toast.makeText(requireContext(), "Audio started playing..", Toast.LENGTH_SHORT).show()
+
+
+//                       }
+//                    }
+//                }
+           })
+        }
 
 
         return root
--- app/src/main/java/com/megahed/eqtarebmenalla/feature_data/presentation/viewoModels/HefzVM.kt
+++ app/src/main/java/com/megahed/eqtarebmenalla/feature_data/presentation/viewoModels/HefzVM.kt
@@ -2,7 +2,7 @@ package com.megahed.eqtarebmenalla.feature_data.presentation.viewoModels
 
 import androidx.lifecycle.LiveData
 import com.megahed.eqtarebmenalla.feature_data.data.remote.hez.entity.ResultHefz
-import com.megahed.eqtarebmenalla.feature_data.data.remote.hez.entity.Reway
+import com.megahed.eqtarebmenalla.feature_data.data.remote.hez.entity.SuraMp3
 import com.megahed.eqtarebmenalla.feature_data.data.repository.HefzRepository
 
 class HefzVM {
@@ -17,5 +17,9 @@ class HefzVM {
 
       return  hefzRepository.getAllRewat()
 
+    }
+
+    fun getSuraMp3(suraId : Int, qar2e : String): LiveData<SuraMp3>{
+        return hefzRepository.getSuraMp3(suraId, qar2e)
     }
 }
